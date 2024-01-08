4f8e724baf076273ddf87ac5c8fce4aee3791382
Badrnej <nejaa.badr@gmail.com>
2024-01-08 15:29:23 -0800

    py1

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,10 +4,10 @@
 
 /**
  *add_nodeint - adds a new node at the beginning of a listint_t list
- *@head: head of listint_t
- *@n: int to add in listint_t list
+ *@head: head of listint_t.
+ *@n: int to add in listint_t list.
  *Return: address of the new element, or NULL if it failed
- */
+*/
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
@@ -22,9 +22,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 }
 /**
  *is_palindrome - identify if a syngle linked list is palindrome
- *@head: head of listint_t
+ *@head: head of listint_t.
  *Return: 1 if it is palindrome else 0
- */
+*/
 int is_palindrome(listint_t **head)
 {
 	listint_t *head2 = *head;
