e881cc61ceda6ec5715b2418954030dff7aa596a
Badrnej <nejaa.badr@gmail.com>
2024-01-22 16:42:05 -0800

    5

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-""" a function that prints x elements of a list """
-    i = nej = 0
+    nej = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             nej += 1
-        except:
-            continue
-    print()
-    return nej
+        except IndexError:
+            break
+    print("")
+    return (nej)
