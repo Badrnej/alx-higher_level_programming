9f2b834d2fcc8fd00f4dd3513db3de55595496f5
Badrnej <nejaa.badr@gmail.com>
2024-01-22 16:34:30 -0800

    3

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-
-    "prints x elements of a list"
+"""
+a function that prints x elements of a list
+"""
     i = pichu = 0
     for i in range(x):
         try:
