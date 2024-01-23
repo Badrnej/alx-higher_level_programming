44224db6423b25bd5bf0ed21925dd1a13f1c58fd
Badrnej <nejaa.badr@gmail.com>
2024-01-22 16:29:21 -0800

    2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+
     "prints x elements of a list"
     i = pichu = 0
     for i in range(x):
@@ -11,4 +12,3 @@ def safe_print_list(my_list=[], x=0):
             continue
     print()
     return pichu
-
