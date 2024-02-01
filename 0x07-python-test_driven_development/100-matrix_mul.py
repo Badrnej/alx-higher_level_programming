bfd8cecfd9d9a25c11270ed6ee268615d637ab84
Badrnej <nejaa.badr@gmail.com>
2024-01-31 18:46:59 -0800

    pytest13

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Defines a matrix multiplication function"""
+"""Defines a matrix multiplication function."""
 
 
 def matrix_mul(m_a, m_b):
     """Multiply two matrices.
     Args:
-        m_a (list of lists of ints/floats): The first matrix
-        m_b (list of lists of ints/floats): The second matrix
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
     Raises:
-        TypeError: If either m_a or m_b is not a list of lists of ints/floats
-        TypeError: If either m_a or m_b is empty
-        TypeError: If either m_a or m_b has different-sized rows
-        ValueError: If m_a and m_b cannot be multiplied
+        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
+        TypeError: If either m_a or m_b is empty.
+        TypeError: If either m_a or m_b has different-sized rows.
+        ValueError: If m_a and m_b cannot be multiplied.
     Returns:
-        A new matrix representing the multiplication of m_a by m_b
+        A new matrix representing the multiplication of m_a by m_b.
     """
 
     if m_a == [] or m_a == [[]]:
@@ -32,10 +32,10 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b must be a list of lists")
 
     if not all((isinstance(ele, int) or isinstance(ele, float))
-            for ele in [num for row in m_a for num in row]):
+               for ele in [num for row in m_a for num in row]):
         raise TypeError("m_a should contain only integers or floats")
     if not all((isinstance(ele, int) or isinstance(ele, float))
-            for ele in [num for row in m_b for num in row]):
+               for ele in [num for row in m_b for num in row]):
         raise TypeError("m_b should contain only integers or floats")
 
     if not all(len(row) == len(m_a[0]) for row in m_a):
